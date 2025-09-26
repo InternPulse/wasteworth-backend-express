2a798448ed2a889a38915871a5acaebede41b93b
oni oluwatosin <oluwatosin.dev@gmail.com>
2025-09-27 00:29:41 +0100

    bug fix

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
-// require('dotenv').config({ path: '.env' });
-// const app = require('./app');
-// const sequelize = require('./config/db');
-
-// const port = process.env.PORT || 3000;
-
-// const startServer = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Database connection successful');
-//     app.listen(port, () => {
-//       console.log(`app listening on port: ${port}.....`);
-//     });
-//   } catch (err) {
-//     console.log('Unable to connect to database', err);
-//   }
-// };
-// startServer();
 require('dotenv').config({ path: '.env' });
 const app = require('./app');
 const sequelize = require('./config/db');
 
-// Initialize database connection (but don't start a server)
-const initializeDatabase = async () => {
+const port = process.env.PORT || 3000;
+
+const startServer = async () => {
   try {
     await sequelize.authenticate();
     console.log('Database connection successful');
+    app.listen(port, () => {
+      console.log(`app listening on port: ${port}.....`);
+    });
   } catch (err) {
-    console.error('Unable to connect to database:', err);
+    console.log('Unable to connect to database', err);
   }
 };
-
-// Initialize the database connection
-initializeDatabase();
-
-module.exports = app;
+startServer();
