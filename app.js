f7c5c8e124af9048301604a8811dc8376d126d37
oni oluwatosin <oluwatosin.dev@gmail.com>
2025-10-06 21:05:34 +0100

    CORS

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookiesParser = require('cookie-parser');
-const helmet = require('helmet')
-const cors = require('cors')
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 
-const AppError = require("./utils/appError");
-const { globalLimiter} = require("./config/rateLimiter");
-const globalErrorHandler = require("./controllers/errorController");
-const ListingRouter = require("./router/listingRouter");
-const MarketPlaceRouter = require("./router/marketplaceRouter");
-const notificationRouter = require("./router/notificationRouter");
-const { protect, restrictTo } = require("./controllers/authController");
+const AppError = require('./utils/appError');
+const { globalLimiter } = require('./config/rateLimiter');
+const globalErrorHandler = require('./controllers/errorController');
+const ListingRouter = require('./router/listingRouter');
+const MarketPlaceRouter = require('./router/marketplaceRouter');
+const notificationRouter = require('./router/notificationRouter');
+const { protect, restrictTo } = require('./controllers/authController');
 
 //development-logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // CORS and Security
 const corsOptions = {
-  origin: ['https://wasteworth-backend-django.onrender.com'],
-  metthods: ['GET', 'POST', 'PUT', 'DELETE'],
-  Headers: ['Content-Type', 'Authorization']
-}
+  origin: [
+    'http://localhost:5173',
+    'https://wasteworth-backend-django.onrender.com',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
-app.use(helmet())
-app.use(cors(corsOptions))
+app.use(helmet());
+app.use(cors(corsOptions));
 //body-parser
 app.use(express.json());
 app.use(cookiesParser());
-
 
 // Apply global rate limiter to all requests
 app.use(globalLimiter);
 
 //routes//
-app.use("/api/v1/listings", ListingRouter);
-app.use("/api/v1/marketplace/listings", MarketPlaceRouter);
-app.use("/api/v1/notifications", notificationRouter);
+app.use('/api/v1/listings', ListingRouter);
+app.use('/api/v1/marketplace/listings', MarketPlaceRouter);
+app.use('/api/v1/notifications', notificationRouter);
 
 app.use((req, res, next) => {
   return next(
--- config/multer.js
+++ config/multer.js
@@ -1,12 +1,12 @@
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinary");
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "wasteworth_listings",
-    allowed_formats: ["jpg", "jpeg", "png"],
+    folder: 'wasteworth_listings',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
   },
 });
 
